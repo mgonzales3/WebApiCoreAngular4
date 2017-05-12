f352aff8c1c33dde067a8f19cc581c20091da8c5
manny gonzales <manny.y.gonzales@gmail.com>
2017-05-11 23:59:45 -0500

    Add files via upload

---
+++ Interfaces/ISample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using SampleCore.Models;
+
+namespace SampleCore.Interfaces
+{
+    public interface ISample
+    {
+        IEnumerable<SamplesViewModel> GetAllSamples();
+        IEnumerable<SamplesViewModel> GetSamplesByStatus(int value);
+        IEnumerable<SamplesViewModel> GetSamplesByUser(string user);
+        Boolean CreateSample(SamplesViewModel newSample);
+    }
+}
---
+++ Interfaces/IUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using SampleCore.Models;
+
+
+namespace SampleCore.Interfaces
+{
+    public interface IUser
+    {
+        IEnumerable<User> GetUsers();
+    }
+}
